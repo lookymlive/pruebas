767c024b079da63768b42e63c42a2c7f166bb194
Lookym <lookymlive@gmail.com>
2023-10-27 18:24:34 -0300

    recordCollection

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
-let count = 0;
+// Configuración
+const recordCollection = {
+  2548: {
+    albumTitle: "Slippery When Wet",
+    artist: "Bon Jovi",
+    tracks: ["Let It Rock", "You Give Love a Bad Name"],
+  },
+  2468: {
+    albumTitle: "1999",
+    artist: "Prince",
+    tracks: ["1999", "Little Red Corvette"],
+  },
+  1245: {
+    artist: "Robert Palmer",
+    tracks: [],
+  },
+  5439: {
+    albumTitle: "ABBA Gold",
+  },
+};
 
-function cc(card) {
-  // Cambia solo el código debajo de esta línea
-
-  let decision;
-
-  switch (card) {
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-      count++;
-      break;
-    case 10:
-    case "J":
-    case "Q":
-    case "K":
-    case "A":
-      count--;
-      break;
-  }
-  if (count > 0) {
-    decision = "Bet";
+// Cambia solo el código debajo de esta línea
+function updateRecords(records, id, prop, value) {
+  if (value === "") {
+    delete records[id][prop];
   } else {
-    decision = "Hold";
+    if (prop === "tracks") {
+      if (!records[id].hasOwnProperty("tracks")) {
+        records[id]["tracks"] = [];
+      }
+      records[id]["tracks"].push(value);
+    } else {
+      records[id][prop] = value;
+    }
   }
-
-  return count + " " + decision;
-
-  // Cambia solo el código encima de esta línea
+  return records;
 }
-
-console.log(cc(2));
+console.log(updateRecords(recordCollection, 5439, "artist", "ABBA"));
+updateRecords(recordCollection, 5439, "artist", "ABBA");
